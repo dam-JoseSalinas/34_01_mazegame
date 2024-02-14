ad48ee860520739136d8f4f82b338d5d31e62ee8
JoseSalinas <2023_jose.salinas@iticbcn.cat>
2024-02-14 10:55:02 +0100

    primeres linies

---
+++ Mazegame.java
@@ -0,0 +1,15 @@
+/*
+Primer execici de la M03-UF4.
+Integrants de la colaboració:
+	- Jose Salinas
+Descripció: 
+
+
+*/
+
+class Mazegame {
+	public static void main(String[] args) {
+		System.out.println("H: mostra ajuda");
+		System.out.println("Laberint: " + args[0]);
+	}
+}
